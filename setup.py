29d494a1613f6e47b135c55e07252bda5210f4aa
ratulalahy <ratul.kuet@gmail.com>
2022-10-17 03:29:50 -0600

    From cookie cutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='BioMedical sentence paraphrasing by finetuning T5 model',
+    author='ratulalahy',
+    license='MIT',
+)
