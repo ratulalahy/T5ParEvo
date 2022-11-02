39dc8448597ac064859e5d8b674bafee8fca848e
ratulalahy <ratul.kuet@gmail.com>
2022-11-02 01:50:36 -0600

    Fixed hydra versioning problem

--- __main__.py
+++ __main__.py
@@ -1,12 +1,22 @@
+from dataclasses import dataclass
+
 import hydra
 from hydra.core.config_store import ConfigStore
+from omegaconf import DictConfig, OmegaConf
 
 from config import SciFactT5Config
 
 conf_store = ConfigStore.instance()
 conf_store.store(name="scifact_t5_config", node=SciFactT5Config)
 
+# cs = ConfigStore.instance()
+# cs.store(name="mnist_config", node=MNISTConfig)
 
-@hydra.main(config_path="conf", config_name="config")
+
+@hydra.main(config_path="conf", config_name="config", version_base=None)
 def main(cfg: SciFactT5Config) -> None:
-    print(cfg.params)
+    print(OmegaConf.to_yaml(cfg))
+
+
+if __name__ == "__main__":
+    main()
--- config.py
+++ config.py
@@ -1,21 +1,22 @@
 from abc import ABC
 from dataclasses import dataclass
 
-# @dataclass 
-# class LocTargetDataset(ABC): 
-#     """
-#     Abstract dataclass for target dataset to attack or to train/test/validate with. 
-#     This class can not be instantiated. 
-#     Inherit this class to show the location of target dataset location.
+
+@dataclass 
+class LocTargetDataset(ABC): 
+    """
+    Abstract dataclass for target dataset to attack or to train/test/validate with. 
+    This class can not be instantiated. 
+    Inherit this class to show the location of target dataset location.
         
-#     Args:
-#         ABC (_type_): Default Abstract class
-#     """
-#     #loc_target_dataset: str
-#     def __new__(cls, *args, **kwargs): 
-#         if cls == LocTargetDataset or cls.__bases__[0] == LocTargetDataset: 
-#             raise TypeError("Cannot instantiate abstract dataclass class.") 
-#         return super().__new__(cls)
+    Args:
+        ABC (_type_): Default Abstract class
+    """
+    #loc_target_dataset: str
+    def __new__(cls, *args, **kwargs): 
+        if cls == LocTargetDataset or cls.__bases__[0] == LocTargetDataset: 
+            raise TypeError("Cannot instantiate abstract dataclass class.") 
+        return super().__new__(cls)
 
 @dataclass
 class ScifactLocTargetDataset:
@@ -31,7 +32,7 @@ class ScifactLocTargetDataset:
     Returns:
         _type_: _description_
     """
-    #loc_target_dataset: str
+    loc_target_dataset: str
     loc_target_dataset_corpus: str
     loc_target_dataset_train: str
     loc_target_dataset_dev: str
