85a575640a818fdc59852812d7073a462d2b5008
ratulalahy <ratul.kuet@gmail.com>
2022-11-02 23:35:47 -0600

    Hydra class config setup

--- __main__.py
+++ __main__.py
@@ -7,13 +7,9 @@ from omegaconf import DictConfig, OmegaConf
 from config import SciFactT5Config
 
 conf_store = ConfigStore.instance()
-conf_store.store(name="scifact_t5_config", node=SciFactT5Config)
+conf_store.store(name="base_config", node=SciFactT5Config)
 
-# cs = ConfigStore.instance()
-# cs.store(name="mnist_config", node=MNISTConfig)
-
-
-@hydra.main(config_path="conf", config_name="config", version_base=None)
+@hydra.main(config_path="conf", config_name="base_config", version_base=None)
 def main(cfg: SciFactT5Config) -> None:
     print(OmegaConf.to_yaml(cfg))
 
--- config.py
+++ config.py
@@ -1,9 +1,12 @@
-from abc import ABC
+import os
 from dataclasses import dataclass
+
+from hydra.utils import get_original_cwd, to_absolute_path
+from omegaconf import MISSING
 
 
 @dataclass 
-class LocTargetDataset(ABC): 
+class TargetDataset: 
     """
     Abstract dataclass for target dataset to attack or to train/test/validate with. 
     This class can not be instantiated. 
@@ -12,14 +15,10 @@ class LocTargetDataset(ABC):
     Args:
         ABC (_type_): Default Abstract class
     """
-    #loc_target_dataset: str
-    def __new__(cls, *args, **kwargs): 
-        if cls == LocTargetDataset or cls.__bases__[0] == LocTargetDataset: 
-            raise TypeError("Cannot instantiate abstract dataclass class.") 
-        return super().__new__(cls)
+    base_dir:str =  MISSING
 
 @dataclass
-class ScifactLocTargetDataset:
+class ScifactTargetDataset(TargetDataset):
     """
     _summary_
 
@@ -32,27 +31,42 @@ class ScifactLocTargetDataset:
     Returns:
         _type_: _description_
     """
-    loc_target_dataset: str
-    loc_target_dataset_corpus: str
-    loc_target_dataset_train: str
-    loc_target_dataset_dev: str
+    base_dir:str = f'{os.getcwd()}/target_system/scifact/data'
+    loc_target_dataset_corpus: str  = f'{base_dir}/corpus.jsonl'
+    loc_target_dataset_train: str  = f'{base_dir}/claims_train.jsonl'
+    loc_target_dataset_dev: str  = f'{base_dir}/claims_dev.jsonl'
 
 
 @dataclass 
-class LocTargetModel(ABC): 
+class TargetModel: 
     """
-    Abstract dataclass for the location of target mode (the model to attack).
-    this abstract class can not be instantiated directly.  
+    Parent dataclass for the location of target mode (the model to attack).
     Inherit this class to show the location of target model.
     Args:
         ABC (_type_): Default Abstract class
     """
-    loc_target_model: str
-    def __new__(cls, *args, **kwargs): 
-        if cls == LocTargetModel or cls.__bases__[0] == LocTargetModel: 
-            raise TypeError("Cannot instantiate abstract dataclass class.") 
-        return super().__new__(cls)
+    base_dir: str  = MISSING
+
 
 @dataclass
+class ScifactTargetModel(TargetModel):
+    """
+    _summary_
+
+    Args:
+        LocTargetDataset (_type_): _description_
+
+    Raises:
+        TypeError: _description_
+
+    Returns:
+        _type_: _description_
+    """
+    base_dir: str =f'{os.getcwd()}/target_system/model'
+    loc_label_model: str = f'{base_dir}/label_roberta_large_fever_scifact'
+    loc_rationale_model: str = f'{base_dir}/rationale_roberta_large_fever_scifact'
+    
+@dataclass
 class SciFactT5Config:
-    loc_target_dataset: ScifactLocTargetDataset
+    target_dataset: TargetDataset = ScifactTargetDataset()
+    target_model: TargetModel = ScifactTargetModel()
